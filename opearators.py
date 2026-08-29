1bbcd5723dee8826a7397f0b1cd79b16177cb2b4
krishna90k9122-netizen <krishna90k9122@gmail.com>
2026-08-29 21:44:37 +0530

    conditional

---
+++ opearators.py
@@ -0,0 +1,11 @@
+num1=int(input("enter a first number:"))
+num2=int(input("Enter a second number:"))
+
+print("FIRST NUMBER:",num1)
+print("Second number:",num2)
+
+print("sum of two numbers:",num1-num2)
+
+print("average of two number:",(num1+num2)/2)
+
+
