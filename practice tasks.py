cd4f45377485a5056894b2d75ec376b7b61cfc08
krishna90k9122-netizen <krishna90k9122@gmail.com>
2026-08-29 19:55:30 +0530

    practice

---
+++ practice tasks.py
@@ -0,0 +1,14 @@
+name= input("ENTER A NAME:")
+
+percentage= input("ENTER A PERCENTAGE:")
+
+age= input("ENTER A AGE:")
+
+college=input("ENTER A COLLEGE NAME:")
+
+
+print("NAME:",name)
+print("age:",age)
+print("percentage:",percentage)
+print("college:",college)
+
