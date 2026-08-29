de3154e80d6ac28b81a63fa153ad6cf81ea10291
krishna90k9122-netizen <krishna90k9122@gmail.com>
2026-08-29 23:16:09 +0530

    practice

---
+++ loops.py
@@ -0,0 +1,6 @@
+num=int(input("Enter a number which you want to write a number = 9"
+""))
+
+for i in range(1,11):
+    
+        print(num , "*" , i ,"=", num*i )     
---
+++ task.py
@@ -0,0 +1,7 @@
+username=input("Enter your username:")
+password=input("Enter your password:")
+
+if(username=="krish" and password=="1234"):
+    print("You are loged in successfully.")
+else:
+    print("Invalid username or password. Please try again.")
