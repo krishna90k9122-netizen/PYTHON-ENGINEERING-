a1d3abc3f9863ac8432e2db458af43332dbf4e09
krishna90k9122-netizen <krishna90k9122@gmail.com>
2026-08-30 23:02:04 +0530

    lists

---
+++ lists.py
@@ -0,0 +1,33 @@
+fruits=["apple", "banana", "orange"]
+
+print(fruits[0])  # Output: apple
+print(fruits[1])  # Output: banana          
+
+print(fruits[2])  # Output: orange  
+
+
+print(fruits[0:2])  # Output: ['apple', 'banana']
+
+fruits=["apple", "banana", "orange"]
+
+fruits[1]="Orange"
+print(fruits)  # Output: ['apple', 'Orange', 'orange']
+
+fruits.append("grapes")
+print(fruits)  # Output: ['apple', 'Orange', 'orange', 'grapes']
+
+fruits.insert(0,"kiwi")
+print(fruits)  # Output: ['kiwi', 'apple', 'Orange', 'orange', 'grapes']
+
+fruits.insert(0,"PINEAPPLE")
+print(fruits)
+
+num=[3,6,8,2,4,4,5,66,7,8,9,10]
+print(sorted(num))
+print(sorted(num, reverse=True))
+
+
+
+
+Z=[1,2,3,4,5,6,7,8,9,10]
+
