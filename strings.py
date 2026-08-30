dd456d5824af18472403e59de79a37a387c496a5
krishna90k9122-netizen <krishna90k9122@gmail.com>
2026-08-30 11:40:30 +0530

    string

--- strings.py
+++ strings.py
@@ -5,4 +5,19 @@ print(name[0])
 print(college[0])   
 
 print(name[0:6])
+print(name[3:6])
+
+name="krishna"
+
+print(name.upper())
+print(name.lower())
+print(name.strip())
+print(len(name))
+
+sentence=input("ENTER WHAT DO YOU LIKE : ")
+
+if "python" in sentence:
+    print("yaaah you are right position")
+else:
+    print("YOU are in wrong position")
 
