46d303eec5a49f606ac44076111ee7c356230a1d
krishna90k9122-netizen <krishna90k9122@gmail.com>
2026-09-05 13:47:06 +0530

    1 prject

---
+++ project1.py
@@ -0,0 +1,13 @@
+Name=input("ENTER YOUR NAME")
+AGE=input("ENTER UR AGE:")
+COLLEGE=input("ENTER YOUR COLLEGE NAME:")
+HEIGHT=input("ENTER YOUR HEIGHT:")
+Is_Students=input("yes or no:")
+
+print("=============STUDENT DETAILS=============")
+print(Name)
+print(AGE)
+print(COLLEGE)
+print(HEIGHT)
+print(Is_Students)
+
